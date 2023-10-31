f231fb92eb1635b83298d98082baed2bec6cc76a
elif <elif.yurtkuran@gmail.com>
2023-10-31 21:40:40 +0300

    flutterproject

--- introapp/lib/main.dart
+++ introapp/lib/main.dart
@@ -24,9 +24,9 @@ class MyProfileWidget extends StatelessWidget {
       width: 398,
       height: 800,
       padding: const EdgeInsets.only(
-        top: 139,
+        top: 145,
         left: 84,
-        right: 85,
+        right: 84,
         bottom: 356,
       ),
       clipBehavior: Clip.antiAlias,
@@ -37,28 +37,16 @@ class MyProfileWidget extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
           SizedBox(
-            width: 201,
-            height: 201,
+            width: 220,
+            height: 180,
             child: Stack(
               children: [
                 Positioned(
-                  left: 0,
+                  left : 14,
                   top: 0,
                   child: Container(
-                    width: 201,
-                    height: 201,
-                    decoration: const ShapeDecoration(
-                      color: Color(0xFFD9D9D9),
-                      shape: OvalBorder(),
-                    ),
-                  ),
-                ),
-                Positioned(
-                  left: 0,
-                  top: 0,
-                  child: Container(
-                    width: 212,
-                    height: 212,
+                    width: 180,
+                    height: 180,
                     decoration: const BoxDecoration(
                       image: DecorationImage(
                         image:
@@ -71,7 +59,7 @@ class MyProfileWidget extends StatelessWidget {
               ],
             ),
           ),
-          const SizedBox(height: 17),
+          const SizedBox(height: 10),
           const SizedBox(
             width: double.infinity,
             child: Column(
