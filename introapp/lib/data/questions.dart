3a63a1f827c3f2092790a1d5064562229b1a017d
elif <elif.yurtkuran@gmail.com>
2023-11-05 13:28:36 +0300

    add Navigator

--- introapp/lib/data/questions.dart
+++ introapp/lib/data/questions.dart
@@ -1,8 +1,15 @@
 import 'package:introapp/models/question_model.dart';
 
 const questions = [
-  QuestionModel("Aşağıdakilerden hangisi bir widget türüdür",
-      ["Stateful", "Constructor", "Class", "Const", "Deneme"]),
-  QuestionModel("2. soru", ["1. şık", "2. şık", "3. şık", "4. şık"])
+  QuestionModel("Flutter nedir?", ["Bir programlama dili", "Bir oyun motoru", "Bir çerçeve", "Bir web tarayıcısı"]),
+  QuestionModel("Flutter hangi şirket tarafından geliştirilmiştir?", ["Google", "Apple", "Facebook", "Microsoft"]),
+  QuestionModel("Hangi programlama dilini Flutter kullanır?", ["Dart", "Python", "Java", "Kotlin"]),
+  QuestionModel("Flutter ile hangi platformlar için uygulama geliştirebilirsiniz?", ["iOS ve Android", "Sadece iOS", "Sadece Android", "Windows"]),
+  QuestionModel("Flutter'da temel bileşen nedir?", ["Widget", "Buton", "Ekran", "Modül"]),
+  QuestionModel("Flutter'ın avantajları nelerdir?", ["Hızlı geliştirme", "Kolay özelleştirme", "Hızlı performans", "Tüm seçenekler"]),
+  QuestionModel("Flutter hangi programlama dilini kullanır?", ["Dart", "Python", "Java", "Kotlin"]),
+  QuestionModel("Flutter'ın popülerliği neden artıyor?", ["Hızlı geliştirme", "Geniş topluluk", "Kolay özelleştirme", "Tüm seçenekler"]),
+  QuestionModel("Flutter ile neler yapabilirsiniz?", ["Mobil uygulamalar", "Web uygulamaları", "Masaüstü uygulamalar", "Tüm seçenekler"]),
+  QuestionModel("Flutter ile uygulama geliştirirken nelere dikkat etmelisiniz?", ["UI tasarımı", "Performans optimizasyonu", "Kod kalitesi", "Tüm seçenekler"]),
 ];
 
--- introapp/lib/main.dart
+++ introapp/lib/main.dart
@@ -2,17 +2,19 @@ import 'package:flutter/material.dart';
 import 'package:introapp/data/questions.dart';
 
 void main() {
-  runApp(const MaterialApp(home: QuizScreen()));
+  runApp(const MaterialApp(
+    home: HomeScreen(),
+  ));
 }
 
 // boilerplate => basmakalıp
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key}); // 1. gereksinim
+  const HomeScreen({Key? key}); // 1. gereksinim
 
   // 2. gereksinim
   // Hot Reload => Restarta gerek kalmadan (spesifik durumlar hariç)
   // değişikliklerin görünmesi.
-  Widget build(BuildContext buildContext) {
+  Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.deepPurpleAccent,
       body: Center(
@@ -33,7 +35,12 @@ class HomeScreen extends StatelessWidget {
                     fontWeight: FontWeight.bold,
                     letterSpacing: 1.5)),
             ElevatedButton(
-              onPressed: () {},
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => QuizScreen()),
+                );
+              },
               style: ElevatedButton.styleFrom(
                   backgroundColor: Colors.amber, foregroundColor: Colors.black),
               child: const Text(
@@ -54,39 +61,45 @@ class HomeScreen extends StatelessWidget {
 
 // 2 class
 // Widget - State
-class QuizScreen extends StatefulWidget {
-  const QuizScreen({super.key});
-
-  @override
-  State<QuizScreen> createState() {
-    return _QuizState();
-  }
-}
 
 // _State
-class _QuizState extends State<QuizScreen> {
-  String text = "Aşağıdakilerden hangisi bir widget türüdür";
 
-  void changeText() {
+class QuizScreen extends StatefulWidget {
+  const QuizScreen({Key? key});
+
+  @override
+  _QuizScreenState createState() => _QuizScreenState();
+}
+
+class _QuizScreenState extends State<QuizScreen> {
+  int currentQuestionIndex = 0;
+
+  void nextQuestion() {
     setState(() {
-      text = "Yeni Değer";
+      if (currentQuestionIndex < questions.length - 1) {
+        currentQuestionIndex++;
+      } else {}
     });
   }
 
   @override
-  Widget build(BuildContext buildContext) {
+  Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text('Quiz App'),
+      ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text(questions[0].question),
-            ...questions[0].answers.map((answer) {
+            Text(questions[currentQuestionIndex].question),
+            ...questions[currentQuestionIndex].answers.map((answer) {
               return ElevatedButton(
-                  onPressed: () {
-                    changeText();
-                  },
-                  child: Text(answer));
+                onPressed: () {
+                  nextQuestion();
+                },
+                child: Text(answer),
+              );
             })
           ],
         ),
