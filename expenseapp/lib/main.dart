f26e629d343459995ef3326aeb5a9e99f6b2f7b7
elif <elif.yurtkuran@gmail.com>
2023-11-09 15:20:03 +0300

    first

---
+++ expenseapp/lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp(home: Scaffold()));
+}
+
+// modelleme
---
+++ expenseapp/lib/models/expense.dart
@@ -0,0 +1,17 @@
+
+// id => kimlik belirleyici
+import 'package:uuid/uuid.dart';
+
+const uuid = Uuid();
+
+class Expense{
+  Expense({required this.name, required this.price, required this.date}) 
+  : id = uuid.v4();
+  final String id;
+  final String name;
+  final double price;
+  final DateTime date;
+}
+
+// id => 1,2,3..
+// String => uuid/guid
