a7201914619bcb79b94415be8999ef4e55949d1d
elif <elif.yurtkuran@gmail.com>
2023-11-23 11:57:37 +0300

    color

--- expenseapp/lib/main.dart
+++ expenseapp/lib/main.dart
@@ -1,7 +1,15 @@
 import 'package:expenseapp/pages/main_page.dart';
 import 'package:flutter/material.dart';
 
-ColorScheme colorScheme1 = ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 59, 18, 222));
+ColorScheme colorScheme1 = ColorScheme.fromSeed(
+  seedColor: Color.fromARGB(255, 59, 18, 222),
+  brightness: Brightness.light,
+  primary: Color.fromARGB(255, 77, 32, 255),
+  secondary: Color.fromARGB(255, 191, 175, 255),
+  background: Colors.white,
+  error: Colors.red,
+
+  );
 
 
 
@@ -23,3 +31,6 @@ void main() {
       )
     );
 }
+
+
+// Color.fromARGB(255, 59, 18, 222)
